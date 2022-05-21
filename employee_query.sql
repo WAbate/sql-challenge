cd54ef36155f015844a49f4a865c4bfe738bc1d9
William Abate <williamabate6@gmail.com>
2022-05-21 13:35:20 -0500

    more tables

--- employee_query.sql
+++ employee_query.sql
@@ -26,3 +26,19 @@ CREATE TABLE employees(
 );
 
 SELECT * from employees;
+
+-- The dept_emp table is many-many relationship, so two primary keys are needed
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
