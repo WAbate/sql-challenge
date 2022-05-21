05c634c2fb67b68d56dcf1bf24ad0740e034b8c8
William Abate <williamabate6@gmail.com>
2022-05-21 14:05:23 -0500

    Update employee_query.sql
    All listings created

--- employee_query.sql
+++ employee_query.sql
@@ -88,3 +88,18 @@ FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_no = 'd007';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT * FROM departments;
+-- The department numbers of Sales and Development departments are d007 and d005
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
