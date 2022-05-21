68598f7c8e330a88eba8954d795541c5b1553e5f
William Abate <williamabate6@gmail.com>
2022-05-21 13:59:45 -0500

    Update employee_query.sql
    Adding Lists

--- employee_query.sql
+++ employee_query.sql
@@ -60,3 +60,31 @@ JOIN salaries ON employees.emp_no = salaries.emp_no;
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%1986';
+
+-- 3. List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+-- 4. List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT * FROM departments;
+-- The department number of Sales department is d007
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd007';
